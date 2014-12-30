b657f81fdebca5ce7392878d348ba2f7e78f388f
Satoshi Iwasaki <yanqirenshi@gmail.com>
2014-12-31 00:58:46 +0900

    modify: Google Cosutom Search API で結果を日付の降順にソートさせる。 close #4

--- src/webapi/google.lisp
+++ src/webapi/google.lisp
@@ -1,7 +1,10 @@
 (in-package :etirwemos)
 
-(defvar *goole-public-api-key*           nil)
-(defvar *google-custom-search-engine-id* nil)
+(defvar *goole-public-api-key* nil
+  "https://www.google.com/cse/all で取得します。")
+
+(defvar *google-custom-search-engine-id* nil
+  "https://www.google.com/cse/all で取得します。")
 
 
 (defun decode-body (body &key (format "json") (external-format :utf-8))
@@ -15,14 +18,14 @@
   ""
   (multiple-value-bind (body-or-stream status-code headers uri stream must-close reason-phrase)
       (drakma:http-request "https://www.googleapis.com/customsearch/v1"
-                           :parameters `(("key" . ,*goole-public-api-key*)
-                                         ("cx"  . ,*google-custom-search-engine-id*)
-                                         ;;("sort" . "item:cacheId")
-                                         ("lr"  . "lang_ja")
-                                         ("alt" . ,format)
-                                         ("num" .   ,(write-to-string num))
+                           :parameters `(("key"   . ,*goole-public-api-key*)
+                                         ("cx"    . ,*google-custom-search-engine-id*)
+                                         ("sort"  . "date")
+                                         ;; ("lr"    . "lang_ja")
+                                         ("alt"   . ,format)
+                                         ("num"   . ,(write-to-string num))
                                          ("start" . ,(write-to-string start))
-                                         ("q"   . ,keyword)))
+                                         ("q"     . ,keyword)))
     (declare (ignore headers uri stream must-close reason-phrase))
     (unless (= 200 status-code)
       (error "error. e=~a,body=~a" status-code (sb-ext:octets-to-string body-or-stream)))
